3fc0496093eacfe2ca0b770785ee5407d27771a8
Lautaro Desouches <lautarodesouches@outlook.com>
2022-06-21 17:45:38 -0300

    Tab Icons

--- src/constants/colors/index.js
+++ src/constants/colors/index.js
@@ -1,6 +1,6 @@
 export const themeBg = '#fff';
 export const themeText = '#000';
-export const primaryBg = '#D16666';
+export const primaryBg = '#034748';
 export const primaryText = '#fff';
 export const secondaryBg = '#ADBABD';
 export const secondaryText = '#000';
--- src/navigation/tab.js
+++ src/navigation/tab.js
@@ -1,26 +1,43 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { primaryBg, primaryText } from '../constants/colors';
 import CartNavigator from './cart';
 import MainNavigator from './main';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const BottomTabs = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <BottomTabs.Navigator initialRouteName='ShopTab'>
+        <BottomTabs.Navigator
+            initialRouteName='ShopTab'
+            screenOptions={{
+                tabBarActiveTintColor: primaryText,
+                tabBarInactiveTintColor: '#d1d1d1',
+                headerShown: false,
+                tabBarLabelStyle: {
+                    fontSize: 18
+                },
+                tabBarStyle: {
+                    backgroundColor: primaryBg,
+                    padding: 10,
+                    height: '7%'
+                }
+            }}
+        >
             <BottomTabs.Screen
                 name='ShopTab'
                 component={MainNavigator}
                 options={{
-                    title: 'Shop',
-                    headerShown: false
+                    tabBarLabel: 'Shop',
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'home' : 'home-outline'} size={20} color={focused ? primaryText : '#d1d1d1'} />
                 }}
             />
             <BottomTabs.Screen
                 name='CartTab'
                 component={CartNavigator}
                 options={{
-                    title: 'Cart',
-                    headerShown: false
+                    tabBarLabel: 'Cart',
+                    tabBarIcon: ({ focused }) => <Ionicons name={focused ? 'cart' : 'cart-outline'} size={20} color={focused ? primaryText : '#d1d1d1'} />
                 }}
             />
         </BottomTabs.Navigator>
